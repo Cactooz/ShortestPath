3b5c8ea20c33a845916580a01b34cfe878113d61
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-11-22 15:34:52 +0100

    Get the stations of the path

--- src/com/company/GetShortestPath.java
+++ src/com/company/GetShortestPath.java
@@ -1,8 +1,22 @@
 package com.company;
 
 public class GetShortestPath {
-	public static int[] Stations() {
 	
+	public static int[] Stations(float[] a, float[][] b, float[] c) {
+		float minDistance = a[0] + b[0][0] + c[0];
+		int[] path = new int[2];
+		
+		for (int m = 0; m < a.length; m++) {
+			for (int n = 0; n < c.length; n++) {
+				float distance = a[m] + b[m][n] + c[n];
+				
+				if (distance < minDistance) {
+					path[0] = m;
+					path[1] = n;
+				}
+			}
+		}
+		return path;
 	}
 	
 	public static float Distance(float[] a, float[][] b, float[] c) {
