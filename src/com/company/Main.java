170f9a7c516a3aa2782f874dc8fe000c61becb0f
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-11-22 13:38:47 +0100

    Base input for width and height of the path system

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+	
+		System.out.print("Hur många steg av stationer: ");
+		int width = input.nextInt();
+		int height;
+	
+		for (int i = 0; i < width; i++) {
+			System.out.print("Stationer på nivå " + (i+1) + ": ");
+			height = input.nextInt();
+		}
     }
 }
