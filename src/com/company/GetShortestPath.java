e7fd0d1ec7226ba9b1bb7112feda08344c6dec8e
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-11-22 15:34:37 +0100

    Get the distance of the path

--- src/com/company/GetShortestPath.java
+++ src/com/company/GetShortestPath.java
@@ -5,7 +5,18 @@ public class GetShortestPath {
 	
 	}
 	
-	public static float Length() {
-	
+	public static float Distance(float[] a, float[][] b, float[] c) {
+		float minDistance = a[0] + b[0][0] + c[0];
+		
+		for (int m = 0; m < a.length; m++) {
+			for (int n = 0; n < c.length; n++) {
+				float distance = a[m] + b[m][n] + c[n];
+				
+				if (distance < minDistance) {
+					minDistance = distance;
+				}
+			}
+		}
+		return minDistance;
 	}
 }
