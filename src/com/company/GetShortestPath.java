8c52e8b4edf53dcc443d8b7e3376efb2ea172775
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-11-23 13:16:39 +0100

    Added comments

--- src/com/company/GetShortestPath.java
+++ src/com/company/GetShortestPath.java
@@ -2,14 +2,20 @@ package com.company;
 
 public class GetShortestPath {
 	
+	//Get the stations passed in the shortest path
 	public static int[] Stations(float[] a, float[][] b, float[] c) {
+		//Set the minDistance to the first elements length
 		float minDistance = a[0] + b[0][0] + c[0];
+		
+		//Make a new array to store the stations
 		int[] path = new int[2];
 		
+		//Loop though all the possible paths
 		for (int m = 0; m < a.length; m++) {
 			for (int n = 0; n < c.length; n++) {
 				float distance = a[m] + b[m][n] + c[n];
 				
+				//Replace the stations if the current distance is smaller
 				if (distance < minDistance) {
 					path[0] = m;
 					path[1] = n;
@@ -19,13 +25,17 @@ public class GetShortestPath {
 		return path;
 	}
 	
+	//Get the distance of the shortest path
 	public static float Distance(float[] a, float[][] b, float[] c) {
+		//Set the minDistance to the first elements length
 		float minDistance = a[0] + b[0][0] + c[0];
 		
+		//Loop though all the possible paths
 		for (int m = 0; m < a.length; m++) {
 			for (int n = 0; n < c.length; n++) {
 				float distance = a[m] + b[m][n] + c[n];
 				
+				//Replace the minDistance if the current distance is smaller
 				if (distance < minDistance) {
 					minDistance = distance;
 				}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,18 +7,23 @@ public class Main {
     public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
+		//Set the width of the system
 		int width = 2;
+		//Make an array with the size of the width
 		int[] height = new int[width];
 	
+		//Ask how many stations should be at each level
 		for (int i = 0; i < width; i++) {
 			System.out.print("Stationer på nivå " + (i+1) + ": ");
 			height[i] = input.nextInt();
 		}
 		
+		//Arrays to store the distance between all the stations
 		float[] a = new float[height[0]];
 		float[][] b = new float[height[0]][height[1]];
 		float[] c = new float[height[1]];
 		
+		//Input the length between the stations
 		System.out.println("Längd mellan stationerna");
 		for (int m = 0; m < a.length; m++) {
 			System.out.print("X -> U" + (m+1) + ": ");
@@ -29,11 +34,13 @@ public class Main {
 			}
 		}
 		
+		//Input the length to the last station
 		for (int n = 0; n < c.length; n++) {
 			System.out.print("V" + (n+1) + " -> Y: ");
 			c[n] = input.nextFloat();
 		}
 		
+		//Print the result
 		System.out.println("Kortaste vägen är: " + GetShortestPath.Distance(a,b,c));
 		System.out.println("Stationerna är: U" + GetShortestPath.Stations(a,b,c)[0] + " och V" + GetShortestPath.Stations(a,b,c)[1]);
     }
